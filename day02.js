5b7458ac1311b32d2af9acb009cc825645ccf977
Svetlana <svetzayats@gmail.com>
2025-12-05 20:56:53 -0500

    Day 2 - Part 2

--- day02.js
+++ day02.js
@@ -12,12 +12,12 @@ const example = `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
 const data = formatData(input);
 
 const invalidIds = data.reduce((acc, cur) => {
-  return [...acc, ...getInvalidIdsForRange(cur)]
+  return [...acc, ...getInvalidIdsForRange(cur, isValidId)]
 }, []);
 
 const res = invalidIds.reduce((acc, cur) => acc + cur, 0);
 console.log('res1', res);
-function getInvalidIdsForRange(range) {
+function getInvalidIdsForRange(range, isValidId) {
   const invalidIds = [];
   let current = range[0];
   while (current <= range[1]) {
@@ -38,7 +38,28 @@ function isValidId(id) {
 }
 
 /** PART 2 */
+console.log('PART 2')
+const invalidIds2 = data.reduce((acc, cur) => {
+  return [...acc, ...getInvalidIdsForRange(cur, isValidId2)]
+}, []);
 
+const res2 = invalidIds2.reduce((acc, cur) => acc + cur, 0);
+console.log('res 2', res2)
+
+function isValidId2(id) {
+  const strId = String(id);
+  if (strId.length === 1) return true;
+  if (new Set(strId.split('')).size === 1) return false;
+  if (!isValidId(id)) return false;
+
+  let suggestedLengthOfSequence = parseInt(strId.length / 2);
+  while (suggestedLengthOfSequence > 1) {
+    const sequence = strId.slice(0, suggestedLengthOfSequence);
+    if (!strId.replaceAll(sequence, '')) return false;
+    suggestedLengthOfSequence--;
+  }
+  return true;
+}
 
 /** PREPARING DATA */
 function formatData(data) {
